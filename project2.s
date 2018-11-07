8e143a46e9e507fc852f26a44663562749e5a290
Kofort9 <kofort9@gmail.com>
2018-11-07 09:39:54 -0500

    file edits
    Project2.asm will be deleted
    add lines to project2.s trying to get loop to work need to do more resaech on how to correctly get and iput of 4 characters via loop.
    then fix the if statment to check the entire input currently put put is in consistent

--- project2.s
+++ project2.s
@@ -9,24 +9,29 @@
 .text
     main:
     # Getting users input 
-    li $v0, 8
-    la $a0, userInput
-    la $a1, 4
-    syscall 
+        li $v0, 8
+        la $a0, userInput
+        la $a1, 4
+        syscall 
+    
+    loop:
+        li $v0, 8      #takes an input
+        syscall
+        li $t0, 0  
+        lb  $t0, ($a0) 
+        li $t1, 10
+    bne $t0, $t1, loop
+
     
     #check if string is empty
-    beq $a0, $zero, stringEmpty  
+    beq $a0, $zero, stringEmpty
+    syscall   
     
     
     #label to print if string is empty
     stringEmpty:
     li $v0, 4
     la $a0, stringStatus
-    syscall 
-    
-    # Display message
-    li $v0, 4
-    la $a0, message
     syscall 
     
     #Display the input 
