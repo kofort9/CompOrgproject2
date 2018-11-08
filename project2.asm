c454e79023edaaa2bbfd0484d9db190b7ff050e4
Kofort9 <kofort9@gmail.com>
2018-11-07 22:55:01 -0500

    some lines were  deleted.
    - refined asking for input
    - created line that will store input in an array
    - created begining of (loop needs work)
    
    -if statement still needs work

--- project2.asm
+++ project2.asm
@@ -1,6 +1,7 @@
 .data 
     stringStatus: .asciiz "Input is empty"
-    userInput: .space 20
+    chars: .space 16
+    userinput: .space 4 
     # add math base 18?
     x: .word 2838581
     n: .word 0
@@ -8,19 +9,26 @@
     
 .text
     main:
-    # Getting users input 
-    	li $v0, 8
-    	la $a0, userInput
-    	la $a1, 4
-    	syscall 
     
+    
+    
+    
+   # Input array
+    la $t0, chars
+    
+    # Get input 1 byte
+    li $v0, 8
+    la $a0, userInput
+    li $a1, 1
+    
+    # stores char in first position of array
+    sw $t1, 0($t0)
+    
+     # loops thru input
     loop:
-    	li $v0, 8      #takes an input
+    	li $t0, userInput      
     	syscall
-    	li $t0, 0  
-    	lb  $t0, ($a0) 
-    	li $t1, 10
-	bne $t0, $t1, loop
+    	
 
     
     #check if string is empty
@@ -37,10 +45,6 @@
     #Display the input 
     li $v0, 4
     la $a0, userInput
-    syscall 
-    
-    # Tell system its the end of main
-    li $v0, 10
     syscall 
     
     
