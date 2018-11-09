26813b698877f6051fe163e1143c110762a95a46
Kofort9 <kofort9@gmail.com>
2018-11-09 17:08:42 -0500

    Begininng of loop
    Add lines to make loop  run 4 time still working on specifics

--- project2.asm
+++ project2.asm
@@ -1,6 +1,6 @@
 .data 
     stringStatus: .asciiz "Input is empty"
-    userinput: .space 5
+    userInput: .space 5
     # add math base 18?
     x: .word 2838581
     n: .word 0
@@ -13,28 +13,32 @@
     li $v0, 8
     la $a0, userInput
     li $a1, 5
+    syscall 
     
-     # loops thru input
+     # loops thru input to check if it is good  
+     # if not end and exit 
     loop:
-    	li $t0, userInput      
+    	li $t1, 0      
     	syscall
     	
 
     
     #check if string is empty
-    beq $a0, $zero, stringEmpty
-    syscall   
+    #beq $a0, $zero, stringEmpty
+    #syscall   
     
     
     #label to print if string is empty
-    stringEmpty:
-    li $v0, 4
-    la $a0, stringStatus
-    syscall 
+    #stringEmpty:
+    #li $v0, 4
+    #la $a0, stringStatus
+    #syscall 
     
-    #Display the input 
-    li $v0, 4
-    la $a0, userInput
-    syscall 
+    # convert input to decimal
+    
+    # convert imput to base 
+    
+    
+    # print output
     
     
--- project2.s
+++ project2.s
@@ -1,10 +1,12 @@
 .data 
     stringStatus: .asciiz "Input is empty"
-    userinput: .space 5
+    userInput: .space 5
     # add math base 18?
     x: .word 2838581
     n: .word 0
     m: .word 0 
+    message: .asciiz "AFter while loop is done"
+    newline: .asciiz "\n"
     
 .text
     main:
@@ -13,28 +15,42 @@
     li $v0, 8
     la $a0, userInput
     li $a1, 5
+    syscall 
     
-     # loops thru input
+     # loops thru input to check if it is good  
+     # if not end and exit 
     loop:
-        li $t0, userInput      
-        syscall
+        bgt $t0, 4, exit 
+        
+        
+        addi $t0, $t0, 1
+        
+        j while
+        
+        
+        
+        
+    exit:
         
 
     
     #check if string is empty
-    beq $a0, $zero, stringEmpty
-    syscall   
+    #beq $a0, $zero, stringEmpty
+    #syscall   
     
     
     #label to print if string is empty
-    stringEmpty:
-    li $v0, 4
-    la $a0, stringStatus
-    syscall 
+    #stringEmpty:
+    #li $v0, 4
+    #la $a0, stringStatus
+    #syscall 
     
-    #Display the input 
-    li $v0, 4
-    la $a0, userInput
-    syscall 
+    # convert input to decimal
+    
+    # convert imput to base 
     
     
+    # print output
+    
+    
+    #end of program
