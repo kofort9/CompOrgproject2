c3714a98eef270bc4d556e241e597fc6e39f98d0
Kofort9 <kofort9@gmail.com>
2018-11-12 20:29:33 -0500

    edits ultimatly not making progress
    I have to make this commit because ive written 5 lines but they don't work. they are supposed to check if the entered sting is too long or empty.  still trying to make progress.

--- project2.s
+++ project2.s
@@ -2,11 +2,8 @@
     stringStatus: .asciiz "Input is empty"
     userInput: .space 5
     # add math base 18?
-    x: .word 2838581
-    n: .word 0
-    m: .word 0 
-    message: .asciiz "AFter while loop is done"
-    newspace: .asciiz " "
+    invaildinputlengthmessage: .asciiz "Input is too long."
+    emptyinput: .asciiz "Input is empty."
     
 .text
     main:
@@ -16,32 +13,74 @@
         la $a0, userInput #loads adress user input  from memory and stores in regester a0
         li $a1, 5 
         syscall 
+        
+        add $t0, $zero, 5
+        
+        # conditional to check if the length of entered string is equal to 4 characters
+        #beqz $a0, inputempty
+        #bne $a0, $t0, stringtoolong
     
         li $v0, 4 
         la $a0, userInput
-        syscall 
+        syscall
+        
+        beqz $a0, inputempty
+        bne $a0, $t0, stringtoolong
+          
+        
+        la $t0, userInput # prints the input from the user
+    lb $a0, ($t0)
+    li $v0, 11
+    syscall
+        
+        
+        
+        
         
         
     
         # loops thru input to check if it is good  
         # if not end and exit 
         loop:
-            bgt $t0, 3, exit # condition statment of the loop
+            bgt $s0, 3, exit # condition statment of the loop
             
             li $v0, 1
-            add $a0, $t0, $zero #  prints the num of iterations
+            add $a0, $s0, $zero #  prints the num of iterations
             syscall 
             # lw $t4, 0($t1)
             # li $v0, 8
             #syscall 
             
         
-            addi $t0, $t0, 1 # i++
+            addi $s0, $s0, 1 # i++
         
             j loop
+            
+         
+            
         
         
-        exit:
         
-    li $v0, 10 
-    syscall 
+        
+        
+        
+    exit:
+        li $v0, 10 
+        syscall 
+    
+    
+    #label to print if string is empty
+    #stringEmpty:
+    #li $v0, 4
+    #la $a0, stringStatus
+    #syscall 
+    
+    # convert input to decimal
+    
+    # convert imput to base 
+    
+    
+    # print output
+    
+    
+    #end of program
